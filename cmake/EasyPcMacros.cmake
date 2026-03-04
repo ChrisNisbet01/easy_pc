3a5e7b35959baee85ba26ee8569a75df1749f1e4
Chris Nisbet <chris@nisbet.kiwi.nz>
2026-03-05 06:14:00 +1300

    Added a BOOTSTRAP_AST option to the GDL generator macro so that users can request that the AST bootstrap code is generated.

--- cmake/EasyPcMacros.cmake
+++ cmake/EasyPcMacros.cmake
@@ -1,7 +1,7 @@
 # Easy PC CMake Macros
 
 function(epc_generate_grammar)
-    set(options)
+    set(options BOOTSTRAP_AST)
     set(oneValueArgs TARGET GDL_FILE OUTPUT_DIR)
     set(multiValueArgs)
     cmake_parse_arguments(EPC_GEN "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
@@ -23,21 +23,33 @@ function(epc_generate_grammar)
     # Get the base name of the GDL file (without extension)
     get_filename_component(GDL_BASE_NAME "${EPC_GEN_GDL_FILE}" NAME_WE)
 
-    set(GENERATED_C "${EPC_GEN_OUTPUT_DIR}/${GDL_BASE_NAME}.c")
-    set(GENERATED_H "${EPC_GEN_OUTPUT_DIR}/${GDL_BASE_NAME}.h")
-    set(GENERATED_ACTIONS_H "${EPC_GEN_OUTPUT_DIR}/${GDL_BASE_NAME}_actions.h")
+    set(GENERATED_SOURCES "${EPC_GEN_OUTPUT_DIR}/${GDL_BASE_NAME}.c")
+    set(GENERATED_HEADERS
+        "${EPC_GEN_OUTPUT_DIR}/${GDL_BASE_NAME}.h"
+        "${EPC_GEN_OUTPUT_DIR}/${GDL_BASE_NAME}_actions.h"
+    )
+
+    set(EXTRA_ARGS)
+    if(EPC_GEN_BOOTSTRAP_AST)
+        list(APPEND EXTRA_ARGS "--bootstrap-ast")
+        list(APPEND GENERATED_SOURCES "${EPC_GEN_OUTPUT_DIR}/${GDL_BASE_NAME}_ast_actions.c")
+        list(APPEND GENERATED_HEADERS
+            "${EPC_GEN_OUTPUT_DIR}/${GDL_BASE_NAME}_ast.h"
+            "${EPC_GEN_OUTPUT_DIR}/${GDL_BASE_NAME}_ast_actions.h"
+        )
+    endif()
 
     # Add custom command to run gdl_compiler
     add_custom_command(
-        OUTPUT "${GENERATED_C}" "${GENERATED_H}" "${GENERATED_ACTIONS_H}"
-        COMMAND gdl_compiler "${EPC_GEN_GDL_FILE}" "--output-dir=${EPC_GEN_OUTPUT_DIR}"
+        OUTPUT ${GENERATED_SOURCES} ${GENERATED_HEADERS}
+        COMMAND gdl_compiler "${EPC_GEN_GDL_FILE}" "--output-dir=${EPC_GEN_OUTPUT_DIR}" ${EXTRA_ARGS}
         DEPENDS "${EPC_GEN_GDL_FILE}" gdl_compiler
         VERBATIM
         COMMENT "Generating parser code for ${GDL_BASE_NAME}.gdl"
     )
 
     # Create a library with the generated sources
-    add_library("${EPC_GEN_TARGET}" STATIC "${GENERATED_C}")
+    add_library("${EPC_GEN_TARGET}" STATIC ${GENERATED_SOURCES})
     target_include_directories("${EPC_GEN_TARGET}" PUBLIC
         "${EPC_GEN_OUTPUT_DIR}"
         "${CMAKE_SOURCE_DIR}/include" # Add easy_pc's public include directory
