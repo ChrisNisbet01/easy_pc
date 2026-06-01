b39994bbf2a4de0e2ec835fd4b489fb73c658182
Chris Nisbet <chris@nisbet.kiwi.nz>
2026-06-02 06:59:13 +1200

    Modified the gdl_lsp project so that it uses the librpc2 project instead of its own RPC2 code.

--- tools/gdl_lsp/gdl_lsp_semantic_tokens.h
+++ tools/gdl_lsp/gdl_lsp_semantic_tokens.h
@@ -1,9 +1,12 @@
 #pragma once
 
-#include "gdl_lsp_server.h"
+#include <json-c/json.h>
+#include <stdbool.h>
+
+typedef struct gdl_lsp_server_st gdl_lsp_server_st;
 
 bool gdl_tokenize_document(gdl_lsp_server_st * svr, char const * uri, char const * text);
 
 void gdl_clear_document_cache(gdl_lsp_server_st * svr, char const * uri);
 
-void gdl_encode_semantic_tokens(gdl_lsp_server_st * svr, char const * uri, struct json_object * id);
+struct json_object * gdl_encode_semantic_tokens(gdl_lsp_server_st * svr, char const * uri);
